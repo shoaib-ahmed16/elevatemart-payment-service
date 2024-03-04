63e71a07c088f157d1ce9883fb5d556da1dcdbd0
shoaib-ahmed16 <sahmed094@gmail.com>
2024-03-04 17:19:08 +0530

    adding the project

---
+++ src/main/java/com/elevatemart/elevatemartpaymentservice/ElevatemartPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elevatemart.elevatemartpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatemartPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatemartPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elevatemart/elevatemartpaymentservice/ElevatemartPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatemart.elevatemartpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
